927ef3959ae5e92cc014e8ebca09d8eff3902539
TanYiBing <820567197@qq.com>
2019-02-19 15:01:42 +0800

    完成分类页面的整体布局

--- src/app/category/category.page.ts
+++ src/app/category/category.page.ts
@@ -5,4 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: 'category.page.html',
   styleUrls: ['category.page.scss']
 })
-export class CategoryPage {}
+export class CategoryPage {
+
+  public recList = [];
+  public cateList = [];
+
+  constructor() {
+    // 右侧分类数据模拟
+    for (let i = 0; i < 10; i++) {
+      this.recList.push({
+        pic: `assets/imgs/0${i}.jpg`,
+        title: `第${i}条`
+      });
+    }
+
+    // 左侧分类数据模拟
+    for (let i = 0; i < 30; i++) {
+      this.cateList.push(`分类${i}`);
+    }
+  }
+}
