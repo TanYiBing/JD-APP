e285dcbfeac02e013760ab2fa230819b42ac8490
TanYiBing <820567197@qq.com>
2019-02-19 14:19:04 +0800

    完成首页顶部轮播图以及精品推荐部分

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,4 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+
+  public recList = [];
+  public bestListWidth = '900px'; /*精品推荐数据长度*/
+  slideOpts = {
+    effect: 'flip',
+    loop: true,
+    autoplay: {
+      delay: 5000,
+    },
+  };
+
+  constructor() {
+    for (let i = 0; i < 10; i++) {
+      this.recList.push({
+        pic: `assets/imgs/0${i}.jpg`,
+        title: `第${i}条`
+      });
+    }
+  }
+
+
+}
