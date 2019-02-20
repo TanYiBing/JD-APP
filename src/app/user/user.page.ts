8068e41e63715a485855a99c37db070ab6b433c1
TanYiBing <820567197@qq.com>
2019-02-20 10:29:30 +0800

    初步完成登陆页面并从用户中心页面导航过来

--- src/app/user/user.page.ts
+++ src/app/user/user.page.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { LoginPage } from '../login/login.page';
 
 @Component({
   selector: 'app-user',
   templateUrl: 'user.page.html',
   styleUrls: ['user.page.scss']
 })
-export class UserPage {}
+
+export class UserPage {
+  constructor(private navCtrl: NavController) {}
+
+  handleLoginClick() {
+    this.navCtrl.navigateForward('/login');
+  }
+}
