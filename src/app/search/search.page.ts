20ba1c49db725dd86a868588946e7bb59b809aa4
TanYiBing <820567197@qq.com>
2019-02-21 09:30:30 +0800

    搜索页面使用storage服务

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../storage.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-search',
@@ -13,7 +15,9 @@ export class SearchPage implements OnInit {
 
   public recList = [];
 
-  constructor() {
+  public historyList = []; /*历史记录的数据*/
+
+  constructor(private storage: StorageService, private alertCtrl: AlertController) {
     for (let i = 0; i < 10; i++) {
       this.recList.push({
         pic: `assets/imgs/0${i}.jpg`,
@@ -25,5 +29,42 @@ export class SearchPage implements OnInit {
   ngOnInit() {
   }
 
+
+  getHistory() {
+    const history = this.storage.get('historyData');
+    if (history) {  /*如果历史记录存在 把历史记录给数据*/
+      this.historyList = history;
+    }
+  }
+  // 删除历史记录
+
+  removeHistory(keywords) {
+    // 提示
+    const confirm = this.alertCtrl.create({
+      title: '您确定要删除吗?',
+      message: '您确定要删除这条历史记录吗，确定点击是，否则点击否。',
+      buttons: [
+        {
+          text: '否',
+          handler: () => {
+
+          }
+        },
+        {
+          text: '是',
+          handler: () => {
+
+            const index = this.historyList.indexOf(keywords);
+            // console.log(index);
+
+            this.historyList.splice(index, 1);
+            // 写入到localstorage
+            this.storage.set('historyData', this.historyList);
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
 
 }
